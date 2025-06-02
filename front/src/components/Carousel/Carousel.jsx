09e3ee087f7f524b94474d65705997d9cd95b1e4
PMoyanoAiraldi <pmoyano497@gmail.com>
2025-06-02 18:48:01 -0300

    Sumando imagen a carousel

--- front/src/components/Carousel/Carousel.jsx
+++ front/src/components/Carousel/Carousel.jsx
@@ -8,6 +8,7 @@ const products = [
     { id: 1, name: 'Soluciones hidráulicas para cada necesidad', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748557815/soluciones_hidraulicas_ynxygl.png' },
     { id: 2, name: 'Comprometidos con el agro, la industria y la movilidad', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748642844/comprometidos_con_el_agro_-_separadas_t4bpyx.png' },
     { id: 3, name: 'Productos importados, calidad garantizada', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748646548/importado_cy32ps.png' },
+    { id: 4, name: 'Cobertura en todo el país, envíos ágiles y seguros', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748900750/camion-logistica-edb_ilyy6w.png' },
     ];
 
 export default function Carousel() {
