dc15554acbafec727e45dcce3ba2684f18448622
PMoyanoAiraldi <pmoyano497@gmail.com>
2025-06-09 20:30:38 -0300

    Editando imagenes y estilos de carousel

--- front/src/components/Carousel/Carousel.jsx
+++ front/src/components/Carousel/Carousel.jsx
@@ -5,9 +5,9 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const products = [
-    { id: 1, name: 'Soluciones hidráulicas para cada necesidad', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748557815/soluciones_hidraulicas_ynxygl.png' },
-    { id: 2, name: 'Comprometidos con el agro, la industria y la movilidad', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748642844/comprometidos_con_el_agro_-_separadas_t4bpyx.png' },
-    { id: 3, name: 'Productos importados, calidad garantizada', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748646548/importado_cy32ps.png' },
+    { id: 1, name: 'Soluciones hidráulicas para cada necesidad', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1749508909/soluciones_hidraulicas_j9n2h0.png' },
+    { id: 2, name: 'Comprometidos con el agro, la industria y la movilidad', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1749510343/comprometidos_con_el_agro_vrvoee.png' },
+    { id: 3, name: 'Productos importados, calidad garantizada', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1749510535/importado_q9qb3m.png' },
     { id: 4, name: 'Cobertura en todo el país, envíos ágiles y seguros', image: 'https://res.cloudinary.com/dl7hjkrhq/image/upload/v1748900750/camion-logistica-edb_ilyy6w.png' },
     ];
 
@@ -26,7 +26,7 @@ const Carousel = () => {
         pauseOnFocus: true,      // pausa si hago foco en los botones
         arrows: true ,            // para que los botones prev/next estén visibles y funcionen
         //centerMode: true,           // para mostrar parte del siguiente slide
-        centerPadding: '10%',       // "piquito" del siguiente
+        //centerPadding: '10%',       // "piquito" del siguiente
     };
 
 
