784f1b6041c6f223ee6e708bb1e4fd45d1ca265b
PMoyanoAiraldi <pmoyano497@gmail.com>
2025-07-16 18:38:27 -0300

    Armar ruta para cambiar contraseña

--- front/src/components/Login/Login.jsx
+++ front/src/components/Login/Login.jsx
@@ -69,7 +69,7 @@ const Login = () =>{
             >
                 <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
         </span>
-        <span className={styles.oldPassword}>¿Olvidaste tu contraseña?</span> 
+        <Link to="#"><span className={styles.oldPassword}>¿Olvidaste tu contraseña?</span> </Link>
         
         <div>
         <Link to="/products"><button type="button" className={styles.button} onClick={() => onLogin(input)} disabled={isButtonDisabled}>Ingresar</button></Link>
