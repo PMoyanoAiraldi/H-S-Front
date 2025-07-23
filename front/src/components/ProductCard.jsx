0733af427cd365d05e159b323a86c11a78202898
PMoyanoAiraldi <pmoyano497@gmail.com>
2025-07-23 19:31:45 -0300

    Empezando a acomodar el componente de Productos

--- front/src/components/ProductCard.jsx
+++ front/src/components/ProductCard.jsx
@@ -1,10 +1,5 @@
-export default function ProductCard({ product }) {
+export default function ProductCard() {
     return (
-        <div className="border rounded-xl p-4 shadow hover:shadow-lg transition">
-            <img src={product.imageUrl} alt={product.name} className="w-full h-40 object-cover rounded-md" />
-            <h2 className="mt-2 font-semibold">{product.name}</h2>
-            <p className="text-gray-600">{product.category}</p>
-            <p className="text-red-600 font-bold">${product.price}</p>
-        </div>
-        );
+        <></>
+    )
     }
